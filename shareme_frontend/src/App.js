ddd7c81758d8c481c502b47e765ad12e9301995a
Juan Espin <juan_espin1995@hotmail.es>
2022-03-01 22:48:59 -0500

    Primer commit

---
+++ shareme_frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect } from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+
+import Login from './components/Login';
+import Home from './container/Home';
+import { fetchUser } from './utils/fetchUser';
+
+const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) navigate('/login');
+  }, [])
+  
+
+  return (
+    <Routes>
+      <Route path="login" element={<Login></Login>}></Route>
+      <Route path="/*" element={<Home></Home>}></Route>
+    </Routes>
+  )
+};
+
+export default App;
